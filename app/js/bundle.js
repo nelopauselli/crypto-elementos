5d6140469eed423f92165761e6b77449ca853b07
Nelo <nelopauselli@gmail.com>
2021-10-27 23:11:09 -0300

    ejecutando claim desde la app

--- app/js/bundle.js
+++ app/js/bundle.js
@@ -11,6 +11,41 @@ async function initWeb3() {
     }
 }
 
+function Element(contract, accountAddress) {
+    let self = this;
+
+    this.contract = contract;
+    this.name = ko.observable("...");
+    this.symbol = ko.observable("...");
+    this.masaAtomica = ko.observable("...");
+    this.balance = ko.observable("...");
+    this.enabled = ko.observable(true);
+
+    this.contract.methods.name().call().then(value => self.name(value));
+    this.contract.methods.symbol().call().then(value => self.symbol(value));
+    //this.contract.methods._masaAtomica().call().then(value => self.masaAtomica(value));
+    self.masaAtomica(1.004)
+    this.contract.methods.balanceOf(accountAddress).call().then(value => self.balance(value));
+    this.accountAddress = accountAddress;
+
+    this.claimable = self.contract.methods.claim !== undefined;
+
+    this.claim = function () {
+        self.contract.methods.claim()
+            .send({
+                from: self.accountAddress,
+                gas: 470000,
+                //value: 1000000000000000000, // in WEI, which is equivalent to 1 ether
+                gasPrice: 0
+            })
+            .then(value => {
+                console.log(value);
+                self.contract.methods.balanceOf(self.accountAddress)
+                    .call()
+                    .then(value => self.balance(value));
+            });
+    }
+}
 
 function ViewModel(accountAddress) {
     var self = this;
@@ -20,26 +55,15 @@ function ViewModel(accountAddress) {
     fetch('./json/hidrogeno.json')
         .then(response => response.json())
         .then(abi => {
-            let address = '0x3bF7f20Bf351C038561c8Cf7aAb0C7C09dEa35dB';
-            let contract = new document.web3.eth.Contract(abi, address);
-            let element = {
-                contract: contract,
-                name: ko.observable("..."),
-                symbol: ko.observable("..."),
-                masaAtomica: 1.008,
-                balance: ko.observable(10000021),
-                enabled: ko.observable(true)
-            }
+            let contract = new document.web3.eth.Contract(abi, '0x3bF7f20Bf351C038561c8Cf7aAb0C7C09dEa35dB');
+            let element = new Element(contract, self.accountAddress)
             self.elementos.push(element);
-            contract.methods.name().call().then(value => element.name(value));
-            contract.methods.symbol().call().then(value => element.symbol(value));
-            contract.methods.balanceOf(self.accountAddress).call().then(value => element.balance(value));
         });
 
-    this.elementos.push({ name: "Helio", symbol: "He", masaAtomica: 4.003, balance: ko.observable(8000), enabled: ko.observable(true) });
-    this.elementos.push({ name: "Carbono", symbol: "C", masaAtomica: 12.01, balance: ko.observable(20), enabled: ko.observable(true) });
-    this.elementos.push({ name: "Nitrógeno", symbol: "N", masaAtomica: 14.0067, balance: ko.observable(0), enabled: ko.observable(true) });
-    this.elementos.push({ name: "Oxígeno", symbol: "O", masaAtomica: 15.9994, balance: ko.observable(0), enabled: ko.observable(false) });
+    // this.elementos.push({ name: "Helio", symbol: "He", masaAtomica: 4.003, balance: ko.observable(8000), enabled: ko.observable(true) });
+    // this.elementos.push({ name: "Carbono", symbol: "C", masaAtomica: 12.01, balance: ko.observable(20), enabled: ko.observable(true) });
+    // this.elementos.push({ name: "Nitrógeno", symbol: "N", masaAtomica: 14.0067, balance: ko.observable(0), enabled: ko.observable(true) });
+    // this.elementos.push({ name: "Oxígeno", symbol: "O", masaAtomica: 15.9994, balance: ko.observable(0), enabled: ko.observable(false) });
 
     this.origen = ko.observable();
     this.cantidad = ko.observable();
@@ -59,24 +83,24 @@ function ViewModel(accountAddress) {
     }
 }
 
-async function loadWalletAsync() {
+function loadWalletAsync() {
     return new Promise(resolve => {
         if (window.ethereum !== undefined) {
             window.ethereum.on('accountsChanged', function (accounts) {
-                console.log(accounts);
                 if (accounts[0]) {
-                    resolve(accounts[0]);
+                    return resolve(accounts[0]);
                 }
             });
 
-            window.ethereum.request({ method: 'eth_requestAccounts' }).then(accounts => {
-                if (accounts.length > 0) {
-                    const account = accounts[0];
-                    if (account) {
-                        resolve(account);
+            window.ethereum.request({ method: 'eth_requestAccounts' })
+                .then(accounts => {
+                    if (accounts.length > 0) {
+                        const account = accounts[0];
+                        if (account) {
+                            return resolve(account);
+                        }
                     }
-                }
-            });
+                });
         }
     });
 }
@@ -85,6 +109,7 @@ async function loadWalletAsync() {
     initWeb3()
         .then(loadWalletAsync)
         .then((address) => {
+            console.log(address);
             var vm = new ViewModel(address);
             console.log(vm);
             ko.applyBindings(vm);
