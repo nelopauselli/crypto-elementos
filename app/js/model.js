f75fced87c11fbe1234aae4a7edee24c031b2c14
Nelo <nelopauselli@gmail.com>
2021-11-30 22:50:38 -0300

    botón para agregar token a metamask

--- app/js/model.js
+++ app/js/model.js
@@ -82,8 +82,37 @@ class ViewModel {
             } else {
                 alert(`balance insuficiente. Usted solo dispone de ${balance} ${origen.name()}`);
             }
-        }
+        };
+
+        this.addToMetamask = async function (token) {
+            const tokenAddress = token.address;
+            const tokenSymbol = token.symbol();
+            const tokenDecimals = 0;
+            const tokenImage = 'http://localhost:5500/img/atom.png';
+
+            try {
+                // wasAdded is a boolean. Like any RPC method, an error may be thrown.
+                const wasAdded = await ethereum.request({
+                    method: 'wallet_watchAsset',
+                    params: {
+                        type: 'ERC20', // Initially only supports ERC20, but eventually more!
+                        options: {
+                            address: tokenAddress, // The address that the token is at.
+                            symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
+                            decimals: tokenDecimals, // The number of decimals in the token
+                            image: tokenImage, // A string url of the token logo
+                        },
+                    },
+                });
+
+                if (wasAdded) {
+                    console.log('Thanks for your interest!');
+                } else {
+                    console.log('Your loss!');
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        };
     }
-
-
 }
