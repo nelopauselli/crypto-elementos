7614ba625922cd3cbbd8c9c18fce462afdc8e0e6
Nelo <nelopauselli@gmail.com>
2021-12-30 19:18:30 -0300

    listando elementos desde la blockchain

--- my-app/src/Dashboard.js
+++ my-app/src/Dashboard.js
@@ -1,46 +1,124 @@
-import React, { Component } from 'react'
-import Element from './Element';
+import React, { Component } from 'react';
+import { ethers } from 'ethers';
 
+import Element from './Element';
 import './Dashboard.css';
+
 
 class Dashboard extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            account: null,
             elements: []
         };
     }
 
-    getData() {
-        // axios.get('http://jsonplaceholder.typicode.com/users').then(res => {
-        //   var data = res.data
-        var data = [
-            { symbol: "H", name: "Hidrogeno", balance: NaN, descrption: 'El elemento original' },
-            { symbol: "He", name: "Helio", balance: NaN, descrption: 'Inflar globos' },
-            { symbol: "Li", name: "Litio", balance: NaN, descrption: 'Usado en baterias' },
-            { symbol: "Be", name: "Berilio", balance: NaN, descrption: 'bla bla' },
-            { symbol: "B", name: "Boro", balance: NaN, descrption: 'bla bla' },
-            { symbol: "C", name: "Carbono", balance: NaN, descrption: 'bla bla' },
-            { symbol: "N", name: "Nitrogeno", balance: NaN, descrption: 'bla bla' },
-            { symbol: "O", name: "Oxigeno", balance: NaN, descrption: 'bla bla' },
-            { symbol: "F", name: "Fluor", balance: NaN, descrption: 'bla bla' },
-            { symbol: "Ne", name: "Neon", balance: NaN, descrption: 'bla bla' },
-            { symbol: "Na", name: "Sodio", balance: NaN, descrption: 'bla bla' },
-            { symbol: "Mg", name: "Magnesio", balance: NaN, descrption: 'bla bla' },
-            { symbol: "Al", name: "Aluminio", balance: NaN, descrption: 'bla bla' },
-            { symbol: "Si", name: "Silicio", balance: NaN, descrption: 'bla bla' },
-        ];
-        this.setState({ elements: data })
-        // })
+    async initWallet() {
+        const { ethereum } = window;
+        if (!ethereum) {
+            alert('No encontramos billetera compatible. :(');
+            return;
+        }
+
+        console.log("usando ethereum");
+
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        console.log(`Encontramos ${accounts.length} direcciones`);
+        if (accounts.length) {
+            this.setState({ account: accounts[0], elements: [] });
+        }
     }
+
+    async getData() {
+        await this.initWallet();
+
+        const { ethereum } = window;
+
+        if (!ethereum) {
+            console.error('No encontramos billetera compatible. :(');
+            return;
+        }
+
+
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+
+        fetch('./json/settings.json')
+            .then(response => response.json())
+            .then(settings => {
+                fetch('./json/root.json')
+                    .then(response => response.json())
+                    .then(async (abi) => {
+                        console.log(abi);
+
+                        let root = new ethers.Contract(settings.root, abi, signer);
+
+                        let elementosSize = await root.contarElementos();
+                        console.log("hay " + elementosSize + " elementos registrados");
+                        let elementosLength = parseInt(elementosSize);
+
+                        let elementos = Array(elementosLength);
+                        for (let index = 0; index < elementosLength; index++) {
+                            let elementAddress = await root.obtenerElemento(index);
+                            console.log('Elemento: ', elementAddress);
+                            elementos[index]=elementAddress;
+                        }
+
+                        let fusionadoresSize = await root.contarFusionadores();
+                        console.log("hay " + fusionadoresSize + " fusionadores registrados");
+                        let fusionadoresLength = parseInt(fusionadoresSize);
+                        
+                        let fusionadores = Array(fusionadoresLength);
+                        for (let index = 0; index < fusionadoresLength; index++) {
+                            let fusionadorAddress = await root.obtenerFusionador(index);
+                            console.log('Fusionador: ', fusionadorAddress);
+                            fusionadores[index]=fusionadorAddress;
+                        }
+
+                        this.setState({account: this.state.account, elements : elementos});
+                    });
+
+                fetch('./json/hidrogeno.json')
+                    .then(response => response.json())
+                    .then(abi => {
+                        // let reward = new Reward(abi, settings.rewards, self.accountAddress);
+                        // self.rewards.push(reward);
+                    });
+
+            });
+
+        
+        // var data = [
+        //     { symbol: "H", name: "Hidrogeno", balance: NaN, descrption: 'El elemento original' },
+        //     { symbol: "He", name: "Helio", balance: NaN, descrption: 'Inflar globos' },
+        //     { symbol: "Li", name: "Litio", balance: NaN, descrption: 'Usado en baterias' },
+        //     { symbol: "Be", name: "Berilio", balance: NaN, descrption: 'bla bla' },
+        //     { symbol: "B", name: "Boro", balance: NaN, descrption: 'bla bla' },
+        //     { symbol: "C", name: "Carbono", balance: NaN, descrption: 'bla bla' },
+        //     { symbol: "N", name: "Nitrogeno", balance: NaN, descrption: 'bla bla' },
+        //     { symbol: "O", name: "Oxigeno", balance: NaN, descrption: 'bla bla' },
+        //     { symbol: "F", name: "Fluor", balance: NaN, descrption: 'bla bla' },
+        //     { symbol: "Ne", name: "Neon", balance: NaN, descrption: 'bla bla' },
+        //     { symbol: "Na", name: "Sodio", balance: NaN, descrption: 'bla bla' },
+        //     { symbol: "Mg", name: "Magnesio", balance: NaN, descrption: 'bla bla' },
+        //     { symbol: "Al", name: "Aluminio", balance: NaN, descrption: 'bla bla' },
+        //     { symbol: "Si", name: "Silicio", balance: NaN, descrption: 'bla bla' },
+        // ];
+        //this.setState({ elements: data })
+    }
+    
     componentDidMount() {
         this.getData()
     }
 
     render() {
         return (
-            <div className="Dashboard-body">
-                {this.state.elements.map(e => (<Element element={e}></Element>))}
+            <div >
+                <div>{this.state.account}</div>
+                <div className="Dashboard-body">
+                    {this.state.elements.map(e => (<Element address={e} account={this.state.account}></Element>))}
+                </div>
             </div>
         );
     }
--- my-app/src/Element.js
+++ my-app/src/Element.js
@@ -1,41 +1,84 @@
 import React, { Component } from 'react'
+import { ethers } from 'ethers';
+
 import "./Element.css";
 
 class Element extends Component {
     constructor(props) {
         super(props);
+        this.setState({
+            name: "loading...",
+            symbol: '?',
+            balance: NaN,
+            description: ''
+        });
+    }
+    getData() {
+        const { ethereum } = window;
+
+        if (!ethereum) {
+            console.error('No encontramos billetera compatible. :(');
+            return;
+        }
+
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+
+        fetch('./json/elemento.json')
+            .then(response => response.json())
+            .then(async (abi) => {
+                console.log(`cargando elemento de la dirección ${this.props.address} para la cuenta ${this.props.account}`);
+                let contract = new ethers.Contract(this.props.address, abi, signer);
+
+                let name = await contract.name();
+                let symbol = await contract.symbol();
+                let balance = await contract.balanceOf(this.props.account);
+                console.log(`Balance of ${name}: ${balance}`);
+
+                this.setState({
+                    contract: contract,
+                    name: name,
+                    symbol: symbol,
+                    balance: parseInt(balance)
+                });
+            });
+    }
+
+    componentDidMount() {
+        this.getData()
     }
 
     addToMetamask(element) {
         alert(`TODO: add token ${element.symbol} to metamask`);
     }
 
-    addToMetamask(element) {
+    fusionar(element) {
         alert(`TODO: fusionar en ${element.symbol}`);
     }
 
     render() {
-        return (
+        const element = this.state;
+        return element ? (
             <div className="Element-card">
                 <img className="Element-icon" src="/logo192.png" alt="..." />
                 <div className="Element-body">
                     <div className="Element-addToMetamask">
-                        <button onClick={() => this.addToMetamask(this.props.element)}>
-                            <img class="img-button" src="img/metamask.png" alt="Agregar a Metamask" title="Agregar a Metamask" />
+                        <button onClick={() => this.addToMetamask(element)}>
+                            <img className="img-button" src="img/metamask.png" alt="Agregar a Metamask" title="Agregar a Metamask" />
                         </button>
                     </div>
-                    <h3>{this.props.element.symbol}</h3>
-                    <h5>{this.props.element.name}</h5>
-                    <div>{this.props.element.balance}</div>
-                    <p>{this.props.element.description}</p>
+                    <h3>{element.symbol}</h3>
+                    <h5>{element.name}</h5>
+                    <div>{element.balance}</div>
+                    <p>{element.description}</p>
                 </div>
                 <div className="Element-footer">
-                    <button class="btn btn-primary" onClick={()=>this.fusionar()}>
+                    <button className="btn btn-primary" onClick={() => this.fusionar()}>
                         Fusionar en <span data-bind="text: $data.to.name"></span>
                     </button>
                 </div>
             </div>
-        );
+        ) : (<div></div>);
     }
 }
 
