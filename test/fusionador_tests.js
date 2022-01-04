f89396ae131b14c256e72e1fb97de316fcb67c52
Nelo <nelopauselli@gmail.com>
2022-01-04 09:29:11 -0300

    tests de 3 fusiones

--- test/fusionador_tests.js
+++ test/fusionador_tests.js
@@ -3,30 +3,19 @@ const Fusionador = artifacts.require("Fusionador");
 const Materia = artifacts.require("Materia");
 const Hidrogeno = artifacts.require("Hidrogeno");
 const Helio = artifacts.require("Helio");
+const Carbono = artifacts.require("Carbono");
 
 const UINT_256_MAX = web3.utils.toBN('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff');
 
 contract("Fusionador", async (accounts) => {
-    const account = accounts[1];
-    const garbage = accounts[2];
-
-    beforeEach(async () => {
-        const hidrogeno = await Hidrogeno.deployed();
-        const helio = await Helio.deployed();
-
-        const balanceHidrogeno = await hidrogeno.balanceOf.call(account);
-        await hidrogeno.transfer(garbage, balanceHidrogeno, {from: account});
-
-        const balanceHelio = await helio.balanceOf.call(account);
-        await helio.transfer(garbage, balanceHelio, {from: account});
-
-        const cosmos = await Cosmos.deployed();
-        await cosmos.claim({ from: account });
-    });
 
     it("Permitir al fusionador gestionar mis Hidrogeno", async () => {
+        const cosmos = await Cosmos.deployed();
         const hidrogeno = await Hidrogeno.deployed();
         const fusionador = await Fusionador.deployed();
+
+        const account = accounts[0];
+        await cosmos.claim({ from: account });
 
         await hidrogeno.approve(fusionador.address, 1000, { from: account });
 
@@ -38,10 +27,10 @@ contract("Fusionador", async (accounts) => {
     it("Desintegrar manual", async () => {
         const hidrogeno = await Hidrogeno.deployed();
         const materia = await Materia.deployed();
+        const cosmos = await Cosmos.deployed();
 
-        const balanceAnterior = await materia.balanceOf.call(account);
-        const expectedAnterior = web3.utils.toBN(0);
-        expect(balanceAnterior).to.eql(expectedAnterior, `El balance de materia ${balanceAnterior.toString()} no es el esperado ${expectedAnterior.toString()}`);
+        const account = accounts[1];
+        await cosmos.claim({ from: account });
 
         await hidrogeno.desintegrar(500, { from: account });
 
@@ -54,6 +43,10 @@ contract("Fusionador", async (accounts) => {
         const hidrogeno = await Hidrogeno.deployed();
         const helio = await Helio.deployed();
         const materia = await Materia.deployed();
+        const cosmos = await Cosmos.deployed();
+
+        const account = accounts[2];
+        await cosmos.claim({ from: account });
 
         await hidrogeno.desintegrar(500, { from: account });
 
@@ -69,6 +62,10 @@ contract("Fusionador", async (accounts) => {
         const hidrogeno = await Hidrogeno.deployed();
         const helio = await Helio.deployed();
         const fusionador = await Fusionador.deployed();
+        const cosmos = await Cosmos.deployed();
+
+        const account = accounts[3];
+        await cosmos.claim({ from: account });
 
         await hidrogeno.approve(fusionador.address, 500, { from: account });
         await fusionador.fusionar(hidrogeno.address, helio.address, 500, { from: account });
@@ -79,6 +76,35 @@ contract("Fusionador", async (accounts) => {
 
         const balanceHidrogeno = await hidrogeno.balanceOf.call(account);
         const expectedHidrogeno = web3.utils.toBN(500);
+        expect(balanceHidrogeno).to.eql(expectedHidrogeno, `El balance de hidrogeno ${balanceHidrogeno.toString()} no es el esperado ${expectedHidrogeno.toString()}`);
+    });
+
+    it("Fusionar Hidrogeno en Helio en Carbono", async () => {
+        const hidrogeno = await Hidrogeno.deployed();
+        const helio = await Helio.deployed();
+        const carbono = await Carbono.deployed();
+        const fusionador = await Fusionador.deployed();
+        const cosmos = await Cosmos.deployed();
+
+        const account = accounts[4];
+        await cosmos.claim({ from: account });
+
+        await hidrogeno.approve(fusionador.address, 1000, { from: account });
+        await fusionador.fusionar(hidrogeno.address, helio.address, 1000, { from: account });
+
+        await helio.approve(fusionador.address, 249, { from: account });
+        await fusionador.fusionar(helio.address, carbono.address, 249, { from: account });
+
+        const balanceCarbono = await carbono.balanceOf.call(account);
+        const expectedCarbono = web3.utils.toBN(83);
+        expect(balanceCarbono).to.eql(expectedCarbono, `El balance de carbono ${balanceCarbono.toString()} no es el esperado ${expectedCarbono.toString()}`);
+
+        const balanceHelio = await helio.balanceOf.call(account);
+        const expectedHelio = web3.utils.toBN(1);
+        expect(balanceHelio).to.eql(expectedHelio, `El balance de helio ${balanceHelio.toString()} no es el esperado ${expectedHelio.toString()}`);
+
+        const balanceHidrogeno = await hidrogeno.balanceOf.call(account);
+        const expectedHidrogeno = web3.utils.toBN(0);
         expect(balanceHidrogeno).to.eql(expectedHidrogeno, `El balance de hidrogeno ${balanceHidrogeno.toString()} no es el esperado ${expectedHidrogeno.toString()}`);
     });
 });
