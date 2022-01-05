38da4eb6bd85d4e11ee470e8d84541bf7f6133ae
Nelo <nelopauselli@gmail.com>
2022-01-05 13:56:10 -0300

    test address

---
+++ app/src/Address.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Address from './Address';
+
+it('text', () => {
+  const content = document.createElement('div');
+  ReactDOM.render(<Address value="0x012345679801234567980123456789" />, content);
+
+  console.log(content.textContent);
+
+  const expected = '0x0123...56789';
+  
+  expect(content.textContent).toBe(expected);
+});
